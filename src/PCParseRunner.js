13170cebc9951268c8e5a01d70427853a716db32
Marc Smith <mr.marcsmith@me.com>
2019-04-26 16:43:03 -0700

    randomized ports to allow parrellel execution

--- src/PCParseRunner.js
+++ src/PCParseRunner.js
@@ -25,6 +25,8 @@ class PCParseRunner {
 		const now = new Date();
 
 		this.seed = now.getTime();
+		this.mongoPort = PCParseRunner.randomPort();
+		this.parsePort = PCParseRunner.randomPort();
 		this.parseVersionValue = '3.1.3';
 		this.mainPath = 'main.js';
 	}
@@ -78,14 +80,22 @@ class PCParseRunner {
 		return '/tmp/testing';
 	}
 
+	static randomIntFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	static randomPort() {
+		return PCParseRunner.randomIntFromInterval(1024, 65535);
+	}
 	async startParseServer() {
 		process.env.TESTING = true;
 
+
 		if (process.env.SPEC_USE_EXTERNAL_SERVER) {
 			Parse.initialize(exampleAppId, exampleJavascriptKey, exampleMasterKey);
-			Parse.serverURL = 'http://localhost:1337/1';
+			Parse.serverURL = 'http://localhost:' + this.parsePort + '/1';
 
-			return;
+			return Parse;
 		}
 
 		const OSType = await PCBash.runCommandPromise('uname -s');
@@ -103,7 +113,7 @@ class PCParseRunner {
 
 		const makeMongo = 'docker run --rm -d ' + this.net + ' ' +
 				'--name mongo-' + this.seed + ' ' +
-				'-p 27017:27017 ' +
+				'-p ' + this.mongoPort + ':27017 ' +
 				'mongo ' +
 				'';
 
@@ -125,12 +135,12 @@ class PCParseRunner {
 		app.appId = exampleAppId;
 		app.masterKey = exampleMasterKey;
 		app.javascriptKey = exampleJavascriptKey;
-		app.port = 1337;
+		app.port = this.parsePort;
 		app.mountPath = '/1';
 		// app.databaseName = PCParseRunner.defaultDBName();
 		// derived data
 		// mac hack
-		app.databaseURI = 'mongodb://' + this.hostURL + ':27017/' + PCParseRunner.defaultDBName();
+		app.databaseURI = 'mongodb://' + this.hostURL + ':' + this.mongoPort + '/' + PCParseRunner.defaultDBName();
 		app.publicServerURL = 'http://localhost:' + app.port + app.mountPath;
 		app.serverURL = app.publicServerURL;
 		app.cloud = '/parse-server/cloud/' + this.mainPath;
@@ -144,13 +154,13 @@ class PCParseRunner {
 		'--name parse-' + this.seed + ' ' +
 		'-v ' + PCParseRunner.tempDir() + '/config-' + this.seed + ':/parse-server/configuration.json ' +
 		'-v ' + PCParseRunner.tempDir() + '/cloud-' + this.seed + ':/parse-server/cloud/ ' +
-		'-p 1337:1337 ' +
+		'-p ' + this.parsePort + ':1337 ' +
 		'parseplatform/parse-server:' + this.parseVersionValue + ' ' +
 		'/parse-server/configuration.json');
 
 		await PCBash.runCommandPromise(
 			'export PC_RUNNER_PARSE_TRIES=10\n' +
-			'until $(curl --output /dev/null --silent --head --fail http://localhost:1337/1/health); do\n' +
+			'until $(curl --output /dev/null --silent --head --fail http://localhost:' + this.parsePort + '/1/health); do\n' +
 			'    printf \'Waiting for Parse Server to come up...\n\'\n' +
 			'    sleep 1\n' +
 			'    ((PC_RUNNER_PARSE_TRIES--))\n' +
@@ -163,7 +173,7 @@ class PCParseRunner {
 
 		await PCBash.runCommandPromise(
 			'export PC_RUNNER_MONGO_TRIES=10\n' +
-			'until $(curl --output /dev/null --silent --fail http://localhost:27017); do\n' +
+			'until $(curl --output /dev/null --silent --fail http://localhost:' + this.mongoPort + '); do\n' +
 			'    printf \'Waiting for Mongo to come up...\n\'\n' +
 			'    sleep 1\n' +
 			'    ((PC_RUNNER_MONGO_TRIES--))\n' +
@@ -175,9 +185,9 @@ class PCParseRunner {
 		);
 
 		Parse.initialize(exampleAppId, exampleJavascriptKey, exampleMasterKey);
-		Parse.serverURL = 'http://localhost:1337/1';
+		Parse.serverURL = 'http://localhost:' + this.parsePort + '/1';
 		// eslint-disable-next-line no-console
-		console.log('Parse Server up and running');
+		console.log('Parse Server up and running ' + Parse);
 
 		return Parse;
 	}
