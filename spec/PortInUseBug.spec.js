803837056ea26e14ad26f2f49fe95cbca8418bbf
Marc Smith <mr.marcsmith@me.com>
2020-03-13 00:49:33 -0700

    Update PortInUseBug.spec.js

--- spec/PortInUseBug.spec.js
+++ spec/PortInUseBug.spec.js
@@ -1,19 +1,6 @@
 const PCParseRunner = require('../src/PCParseRunner.js');
 
-describe('test Mongo inserts', () => {
-	it('should fail', () => {
-		expect.assertions(1);
-
-		for (let i = 5000 - 1; i >= 0; i--) {
-			const val1 = Math.random();
-			const val2 = Math.random();
-
-			if (val1 === val2) {
-				expect(1).toBe(2);
-			}
-		}
-		expect(1).toBe(1);
-	}, 2 * 60 * 1000);
+describe('test randomPort race condition', () => {
 	it('should avoid duplicate ports', () => {
 		expect.assertions(1);
 
