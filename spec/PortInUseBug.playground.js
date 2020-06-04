e0e416fe004536afe3dbe78177408ae62b0b88ff
Marc Smith <mr.marcsmith@me.com>
2020-06-04 00:44:27 -0700

    removed PortInUseBug.spec because contains randomness

---
+++ spec/PortInUseBug.playground.js
@@ -0,0 +1,19 @@
+// this is just a playground
+// Randomness doesn't belong in a test suite
+// const PCParseRunner = require('../src/PCParseRunner.js');
+
+// describe('test randomPort race condition', () => {
+// 	it('should avoid duplicate ports', () => {
+// 		expect.assertions(1);
+
+// 		for (let i = 5000 - 1; i >= 0; i--) {
+// 			const val1 = PCParseRunner.randomPort();
+// 			const val2 = PCParseRunner.randomPort();
+
+// 			if (val1 === val2) {
+// 				expect(1).toBe(2);
+// 			}
+// 		}
+// 		expect(1).toBe(1);
+// 	}, 2 * 60 * 1000);
+// });
